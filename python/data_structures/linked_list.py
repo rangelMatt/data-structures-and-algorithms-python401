6a7290d2fbd5ccd89a1c4e59501a52a4ff0ecfca
Matt Rangel <mattrangel@gmail.com>
2022-04-25 22:39:02 -0700

    completed

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -7,6 +7,7 @@ class LinkedList:
     def __init__(self):
         # initialization here
         self.head = None
+        self.next = None
 
     def __str__(self):
         # create result
@@ -17,6 +18,49 @@ class LinkedList:
             current = current.next
         return return_str + "NULL"
 
+    def append(self, value):
+        new_node = Node(value)
+        # if self.head is None:
+        #     self.head = new_node
+        #     return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def insert_before(self,value,new_value):
+        if not self.head:
+            raise TargetError
+
+        if self.includes(value) is False:
+            raise TargetError
+
+        current = self.head
+
+        if current.value == value:
+            self.head = Node(new_value, current)
+            return
+        while current is not None:
+
+            if value == current.next.value:
+                current.next = Node(new_value, current.next)
+                return
+            current = current.next
+
+    def insert_after(self,value,new_value):
+        if not self.head:
+            raise TargetError
+
+        if self.includes(value) is False:
+            raise TargetError
+
+        current = self.head
+        while current is not None:
+
+            if value == current.value:
+                current.next = Node(new_value, current.next)
+                return
+            current = current.next
 
     def insert(self,value):
         # creating a new Node with the correct value
@@ -40,5 +84,5 @@ class Node:
         self.value = value
         self.next = next
 
-class TargetError:
+class TargetError(Exception):
     pass
