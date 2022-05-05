89d214f61693ed225fb50f853687d9244a5bb487
Matt Rangel <95889943+rangelMatt@users.noreply.github.com>
2022-05-05 10:21:30 -0700

    Revert "Completed CC 13"

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,42 +1,5 @@
-from data_structures.stack import Stack
+from data_structures.queue import Queue
 
 
-def multi_bracket_validation(string):
-    brackets = {']':'[','}':'{',')':'('}
-    new_stack = Stack()
-    for char in string:
-        if char not in brackets:
-            new_stack.push(char)
-        else:
-            if new_stack.top == None:
-                return False
-                break
-            if new_stack.top.value == brackets[char]:
-                new_stack.pop()
-            else:
-                return False
-                break
-    if new_stack.is_empty:
-        return True
-    else:
-        return False
-
-
-# def multi_bracket_validation(string):
-#     stack = []
-
-#     for paren in string:
-#         if paren == '(' or paren == '[' or paren == '{':
-#             stack.append(paren)
-#         else:
-#             if not stack:
-#                 return False
-#             else:
-#                 top = stack[-1]
-#                 if paren == ')' and top == '(' or paren ==']' and top == '[' or paren == '}' and top == '{':
-#                     stack.pop()
-
-#     if not stack:
-#         return True
-#     else:
-#         return False
+def multi_bracket_validation():
+    pass
--- python/tests/code_challenges/test_stack_queue_brackets.py
+++ python/tests/code_challenges/test_stack_queue_brackets.py
@@ -2,85 +2,64 @@ import pytest
 from code_challenges.stack_queue_brackets import multi_bracket_validation
 
 
-
+@pytest.mark.skip("TODO")
 def test_validates_two_square_brackets():
     actual = multi_bracket_validation("[]")
     expected = True
     assert actual == expected
 
 
-# @pytest.mark.skip("TODO")
+@pytest.mark.skip("TODO")
 def test_fails_two_square_brackets_flipped():
     actual = multi_bracket_validation("][")
     expected = False
     assert actual == expected
 
 
-# @pytest.mark.skip("TODO")
+@pytest.mark.skip("TODO")
 def test_validates_two_braces():
     actual = multi_bracket_validation("{}")
     expected = True
     assert actual == expected
 
 
-# @pytest.mark.skip("TODO")
+@pytest.mark.skip("TODO")
 def test_fails_two_braces_flipped():
     actual = multi_bracket_validation("}{")
     expected = False
     assert actual == expected
 
 
-# @pytest.mark.skip("TODO")
+@pytest.mark.skip("TODO")
 def test_validates_two_parentheses():
     actual = multi_bracket_validation("()")
     expected = True
     assert actual == expected
 
 
-# @pytest.mark.skip("TODO")
+@pytest.mark.skip("TODO")
 def test_fails_two_parentheses_flipped():
     actual = multi_bracket_validation(")(")
     expected = False
     assert actual == expected
 
 
-# @pytest.mark.skip("TODO")
+@pytest.mark.skip("TODO")
 def test_multi():
     actual = multi_bracket_validation("{}(){}")
     expected = True
     assert actual == expected
 
 
-# @pytest.mark.skip("TODO")
+@pytest.mark.skip("TODO")
 def test_nested():
     actual = multi_bracket_validation("{([])}")
     expected = True
     assert actual == expected
 
 
-
+@pytest.mark.skip("TODO")
 def test_mismatched():
     actual = multi_bracket_validation("[}")
     expected = False
-    assert actual == expected
-
-
-def test_not_nested_():
-    actual = multi_bracket_validation("[({}]")
-    expected = False
-    assert actual == expected
-
-def test_not_nested_too():
-    actual = multi_bracket_validation("(](")
-    expected = False
-    assert actual == expected
-
-def test_not_nested_thrice():
-    actual = multi_bracket_validation("{(})")
-    expected = False
-    assert actual == expected
-
-def test_two_normal_one_nest():
-    actual = multi_bracket_validation("(){}[[]]")
-    expected = True
     assert actual == expected
