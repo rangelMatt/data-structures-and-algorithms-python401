7278131707034d34b6a103a99ab9d38302049d3b
Matt Rangel <mattrangel@gmail.com>
2022-05-10 19:03:26 -0700

    Completed CC #17

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,28 @@
-from data_structures.binary_tree import BinaryTree
+
+from data_structures.queue import Queue
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    result = []
+
+    if tree is None:
+        return result
+    if tree.root is None:
+        return result
+
+    b_que = Queue()
+    b_que.enqueue(tree.root)
+
+    while not b_que.is_empty():
+        front_node = b_que.dequeue()
+        result.append(front_node.value)
+
+        if front_node.left is not None:
+            b_que.enqueue(front_node.left)
+
+        if  front_node.right is not None:
+            b_que.enqueue(front_node.right)
+
+    return result
+
+
